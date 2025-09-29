1a5aceb9bf2ba5ea82b895c28ea26793e9d02679
farhadtanveer <farhadtanveer05@gmail.com>
2025-09-29 08:55:09 +0200

    test commit after cleanup

--- backend/cmd/serve.go
+++ backend/cmd/serve.go
@@ -2,20 +2,33 @@ package cmd
 
 import (
 	"ecommerce/config"
+	"ecommerce/infra/db"
+	"ecommerce/repo"
 	"ecommerce/rest"
 	"ecommerce/rest/handlers/product"
 	"ecommerce/rest/handlers/user"
 	middleware "ecommerce/rest/middleswares"
+	"fmt"
 )
 
 func Serve() {
 	cnf := config.GetConfig()// Load configuration
+	dbCon, err := db.NewConnection() // Initialize database connection
+	if err != nil {
+		fmt.Println("Failed to connect to the database:", err)
+	}
 
 	middlewares := middleware.NewMiddlewares(cnf) // Initialize middlewares
 
-	productHandler := product.NewHandler(middlewares) // Initialize handlers
-	userHandler := user.NewHandler()
+	// Initialize repositories
+	productRepo := repo.NewProductRepo() 
+	userRepo := repo.NewUserRepo(dbCon)
 
-	server := rest.NewServer(cnf, productHandler, userHandler)      // Start the server
+	// Initialize handlers
+	productHandler := product.NewHandler(middlewares, productRepo)
+	userHandler := user.NewHandler(cnf, userRepo)
+
+	// Start the server
+	server := rest.NewServer(cnf, productHandler, userHandler)      
 	server.Start()
 }
